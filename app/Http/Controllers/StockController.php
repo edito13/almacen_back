9d774a6bd9019742260babc4a26cd7bd9bd29213
Edito Tchokoso <editotchocosso13@gmail.com>
2025-05-22 11:32:13 +0100

    New changes

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class StockController extends Controller {
     public function index() {
         // Retorna todos os equipamentos com a categoria (se quiser)
-        $equipments = Equipment::with('category')->get();
+        $equipments = Equipment::with(['entries', 'exits', 'category'])->get();
 
         return response()->json($equipments);
     }
